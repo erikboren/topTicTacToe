b0c350cbe7de91b5d26000d9f579f9178199ec4e
erikboren <erik.boren@gmail.com>
2021-11-21 19:40:06 +0100

    Now, fields can't be played twice! + design

--- index.js
+++ index.js
@@ -19,16 +19,10 @@ const playerFactory = (marker,isBot,playerID) =>{
 
 const fieldFactory = (xCord,yCord) =>{
     this.status = null;
-    
    const cord = [xCord,yCord];
 
     const playField = function(player){
-        if (status != null){
-            this.status = player.marker;
-            return true;
-        } else {
-            return false;
-        }
+        this.status = player.marker;
     };
 
     return {status, cord, playField};
@@ -37,11 +31,19 @@ const fieldFactory = (xCord,yCord) =>{
 
 const screenFieldFactory = (xCord,yCord) =>{
     var element = document.createElement('div');
+    
     element.classList.add('screenField');
+    
+    const cordSum = xCord+yCord+1;
+    
+    if(cordSum % 2 == 1){
+        element.classList.add("oddField");
+    } else{
+        element.classList.add("evenField");
+    }
+    
     element.onclick = function(){
-        element.textContent = players[activePlayer].marker;
         players[activePlayer].play(xCord,yCord);
-        
     };
     return  {element};
 };
@@ -53,22 +55,25 @@ players[1] = playerFactory('o',false,1);
 
 const board = (function(){
     let fieldArray =  Array.from(Array(3), () => new Array(3));
-    let markerArray = Array.from(Array(3), () => new Array(3));
     
     
     for(i=0;i<3;i++){
         for(k=0; k<3; k++){
             var field = fieldFactory(i,k);
             fieldArray[i][k] = field;
-            markerArray[i][k] = field.status;
         }
     }
 
     const placeMarker = function(xCord,yCord,player){
-        fieldArray[xCord][yCord].playField(player);
-        activePlayer = changePlayer(player);
-        var win = checkWin(player);
-        win[0] == true ? screenController.displayWinner(player.playerID): screenController.displayPlayerTurn() ;
+        if(fieldArray[xCord][yCord].status == 'null'){
+            fieldArray[xCord][yCord].playField(player);
+            activePlayer = changePlayer(player);
+            var win = checkWin(player);
+            win[0] == true ? screenController.displayWinner(player.playerID): screenController.displayPlayerTurn();
+            screenController.updateField(xCord,yCord);
+        } else{
+            console.log(fieldArray[xCord][yCord].status);
+        }
         return;
     };
 
@@ -104,23 +109,22 @@ const board = (function(){
     const changePlayer = function(player){
         return player.playerID == 0 ? 1 : 0;
     };
-    const printMArray = () => {
-        console.table(markerArray);
-    };
+
 
     const printArray = ()=> console.table(fieldArray);
 
-    return {printArray, fieldArray, placeMarker, printMArray, markerArray, checkWin};
+    return {printArray, fieldArray, placeMarker, checkWin};
 
 })();
 
 const screenController = (function(){
-    
+    var screenFieldArray = Array.from(Array(3), () => new Array(3));
     const display = function(){
         screenBoard.innerHTML = '';
         for(i=0;i<3;i++){
         for(k=0; k<3; k++){
-           screenBoard.appendChild(screenFieldFactory(i,k).element); 
+            screenFieldArray[i][k] = screenFieldFactory(i,k).element;
+           screenBoard.appendChild(screenFieldArray[i][k]); 
             }
         }
     };
@@ -140,7 +144,17 @@ const screenController = (function(){
         displayPlayerTurn();
     };
 
-    return {update, displayPlayerTurn, displayWinner};
+    const updateField = function(xCord,yCord){
+        screenFieldArray[xCord][yCord].textContent = board.fieldArray[xCord][yCord].status;
+        screenBoard.innerHTML = '';
+        for(i=0;i<3;i++){
+            for(k=0; k<3; k++){
+               screenBoard.appendChild(screenFieldArray[i][k]); 
+                }
+            }
+    };
+
+    return {update, displayPlayerTurn, displayWinner, updateField};
 })();
 
 
@@ -148,7 +162,3 @@ const screenController = (function(){
 var activePlayer = 0;
 
 screenController.update();
-
-board.printMArray();
-
-board.printArray();
